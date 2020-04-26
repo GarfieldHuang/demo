3f59bcd1d3f8c736fa8cec508d60157822d81a3e
黄大胖子 <82988601@qq.com>
2020-04-26 21:50:56 +0800

    更新

---
+++ SpringBoot/springboot-springbootdataredis-redis/src/test/java/xyz/hdpz/springbootspringbootdataredisredis/SpringbootSpringbootdataredisRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.hdpz.springbootspringbootdataredisredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSpringbootdataredisRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
